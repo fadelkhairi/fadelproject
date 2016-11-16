abb29517a4aba50901e9021010677e27c7bcdf08
fadelkhairi <fadel.jtk12@gmail.com>
2016-11-16 20:56:00 +0700

    no message

---
+++ config/env/mongo.js
@@ -0,0 +1,25 @@
+{
+   "mongodb": [
+      {
+         "name": "mongodb-76b24",
+         "label": "mongodb",
+         "tags": [
+           "nosql",
+           "document",
+           "data_management",
+           "ibm_experimental"
+          ],
+         "plan": "100",
+         "credentials": {
+            "hostname": "10.0.116.49",
+            "host": "10.0.116.49",
+            "port": 10001,
+            "username": "be879069-b273-4656-b5fb-3daa5c508044",
+            "password": "f268582e-0a52-42a8-9b97-66889a9cb662",
+            "name": "76ea370c-8678-4c51-b3cf-a0cd722ed93a",
+            "db": "db",
+            "url": "mongodb://be879069-b273-4656-b5fb-3daa5c508044:f268582e-0a52-42a8-9b97-66889a9cb662@10.0.116.49:10001/db"
+         }
+      }
+   ]
+}
